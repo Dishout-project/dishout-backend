739534821cf051054fb7fc9cfccadd3da2cd2b3a
realcodeworld <realcodeworld@gmail.com>
2020-09-14 18:34:10 +0100

    update routes

--- routes/dishes.js
+++ routes/dishes.js
@@ -3,30 +3,43 @@ let Dish = require("../models/dish.model");
 
 router.route("/").get((req, res) => {
     Dish.find()
-        .then(dishes => res.json(dishes))
+        .then(dishes => {
+            res.json({dishes: dishes});
+            res.status(200);
+        })
         .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.route("/add").post((req, res) => {
     const dish = req.body.dish;
-
+    console.log('adding ' + dish)
     const newDish = new Dish({dish});
 
     newDish.save()
-        .then(() => res.json("Dish added!"))
+        .then(() => {
+            res.json("Dish added!");
+            res.status(201);
+        })
         .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.route("/:id").get((req, res) => {
     Dish.findById(req.params.id)
-        .then(dish => res.json(dish))
+        .then(dish => {
+            res.json(dish);
+            res.status(200);
+        })
         .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.route("/:id").delete((req, res) => {
     Dish.findByIdAndDelete(req.params.id)
-        .then(() => res.json("Dish deleted"))
-        .catch(err => res.status(400).json("Error: " + err));
+        .then(() => {
+            // res{status: 204};
+            // res.json("Dish deleted");
+            res.status(204).send({ json: "Dish deleted!" });
+        })
+        .catch(err => res.status(400).send("Error: " + err));
 });
 
 router.route("/:id").post((req, res) => {
@@ -35,7 +48,10 @@ router.route("/:id").post((req, res) => {
             dish.dish = req.body.dish;
 
             dish.save()
-                .then(() => res.json("Dish updated!"))
+                .then(() => {
+                    res.json("Dish updated!");
+                    res.status(200);
+                })
                 .catch(err => res.status(400).json("Error: " + err));
         })
         .catch(err => res.status(400).json("Error: " + err));
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+console.log('port: ' + process.env.PORT)
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb://localhost:27017/myapp';
+console.log('uri: ' + uri)
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
